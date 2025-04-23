883579ad4f34c072469beb86ab457827548fc4e1
serensz <punnza145@gmail.com>
2025-04-23 16:11:22 +0700

    fix prompt + tuning chain

--- rag_chain.py
+++ rag_chain.py
@@ -1,24 +1,36 @@
-from langchain.chains import RetrievalQA 
+from langchain.chains import RetrievalQA
 from langchain_core.runnables import RunnableLambda, RunnablePassthrough
 from langchain_community.llms import Ollama
+from store import store_all_pdfs_in_docs  
 
-def build_chain(vectorstore):
-    ollama_llm = Ollama(model="llama3.2",temperature=0.7)
-
-    retriever = vectorstore.as_retriever()
+def build_chain():
+    vectorstore = store_all_pdfs_in_docs()
+    ollama_llm = Ollama(model="mistral:latest", temperature=0.2)
+    retriever = vectorstore.as_retriever(search_type="similarity", search_kwargs={"k": 1})
 
     def format_docs(docs):
         return "\n\n".join([doc.page_content for doc in docs])
     
-    def character_prompt(question):
-        character = ("You are the assistant at Prince of Songkla University, Computer Science Department")       
-        return character + question
-     
-    chain=(
-        RunnableLambda(lambda x: x["question"]) |
-        retriever |
-        RunnableLambda(format_docs) |
-        RunnableLambda(character_prompt) |
+    def create_prompt(question_and_context):
+        question = question_and_context["question"]
+        context = question_and_context["context"]
+
+        prompt = f"""
+        You are a developer for Typhoon LLM model, you need to explain me how things are working depend on what user asked
+
+        Document:
+        {context}
+
+        Question:
+        {question}
+
+        Answer:
+        """
+        return prompt.strip()
+    
+    chain = (
+        RunnableLambda(lambda x: {"question": x["question"], "context": format_docs(retriever.invoke(x["question"]))}) |
+        RunnableLambda(create_prompt) |
         ollama_llm
     )
     return chain
